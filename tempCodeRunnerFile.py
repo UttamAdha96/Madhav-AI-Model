e77d16e9c707bc3880e51a950240980b1a3fcbf1
Uttam Adha <21BCS8411@cuchd.in>
2023-12-25 09:54:06 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+self.query = self.query.replace("")
+                # Youresult= "https://www.youtube.com/results?search_query=" + self.query
+                # webbrowser.open(Youresult)
+                # pywhatkit.playonyt(Youresult)
+                # speak("Opening Youtube sir please wait..")
